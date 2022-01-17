9bad22d8eeee74914f5c6102deb6f2fdc73bb433
Jason <a0918791130@gmail.com>
2022-01-17 13:24:09 +0800

    Add PWA feature

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import WeatherApp from "./WeatherApp";
 
+import * as serviceWorker from './serviceWorker';
+
 // 這支 CSS 檔的樣式會作用到全域
 import "./styles.css";
 
@@ -12,3 +14,6 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// 將 PWA 中的 Service Worker 功能打開
+serviceWorker.register();
